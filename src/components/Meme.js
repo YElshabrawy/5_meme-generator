cb32d534318e8938815062c4cc1e12311afcb2c1
Youssef Elshabrawy <yielshabrawy@gmail.com>
2022-08-05 08:26:05 +0200

    finsl product

---
+++ src/components/Meme.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import memesData from '../data/memesData';
+
+export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage: 'http://i.imgflip.com/1bij.jpg',
+    });
+
+    /* eslint-disable */
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url;
+        setMeme((prevMeme) => ({
+            ...prevMeme,
+            randomImage: url,
+        }));
+    }
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+
+        setMeme((prev) => {
+            return {
+                ...prev,
+                [name]: value,
+            };
+        });
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    onChange={handleChange}
+                />
+                <button className="form--button" onClick={getMemeImage}>
+                    Get a new meme image 🖼️
+                </button>
+            </div>
+            <div className="meme">
+                <img alt="" src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
+    );
+}
